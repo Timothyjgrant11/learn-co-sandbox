498cb2bd644f5db577128c92486f9e647b300673
Timothy Grant <timothyjgrant11@gmail.com>
2018-09-30 19:18:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,6 @@ def badge_maker
   puts "Hello, my name is #{name}
 end
 
-names = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 

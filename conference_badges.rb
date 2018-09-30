1f0e3616fd7642cc79307c4b136e02c639cfa597
Timothy Grant <timothyjgrant11@gmail.com>
2018-09-30 19:32:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 badge_maker(name)
-   puts "Hello, my name is #{name}
+   puts "Hello, my name is #{name}"
 names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 names.each do |name|
